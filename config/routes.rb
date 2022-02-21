5c4af127b9a3a95181ba6c56dbb42d779aa0803e
EC2 Default User <ec2-user@ip-172-31-95-155.ec2.internal>
2022-02-21 11:08:59 +0000

    [Finish]RSpecテスト実施・完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  resources :books, except: [:destroy]
-  delete 'book/:id' => 'books#destroy', as: 'destroy_book'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
